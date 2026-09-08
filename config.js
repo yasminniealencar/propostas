364ea598d7ded70dfd0290dc11605ef8e1dd71b3
yasminniealencar <yasminalencar1997@gmail.com>
2026-09-08 16:57:36 -0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 window.INOVAR_CONFIG = {
   supabaseUrl: 'https://kmussdssbssgkvxcqlsk.supabase.co',
   supabaseAnonKey: 'sb_publishable_7ZmxNOtnXAzJeOiJ6UH0Nw_ZYzSG9fx',
-  publicBaseUrl: 'https://yasminniealencar.github.io/propostas',
+  publicBaseUrl: 'https://propostas-inovargondolas.pages.dev',
   authEmailDomain: 'inovargondolas.app',
   demoMode: false
 };
